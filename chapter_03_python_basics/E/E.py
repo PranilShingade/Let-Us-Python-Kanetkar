1b9291ed87ea3bd7d2c357d808eafa2ed8661555
Pranil Shingade <pratik.pranilshingade@gmail.com>
2026-01-22 17:59:27 +0530

    chapter 3 python basics complete, chapter 4 strings starts

---
+++ chapter_03_python_basics/E/E.py
@@ -0,0 +1,14 @@
+#Evaluate the following expressions: 
+# (a) min(2, 6, 8, 5) 
+# (b) bin(46) 
+# (c) round(10.544336, 2) 
+# (d) math.hypot(6, 8) 
+# (e) math.modf(3.1415)
+
+import math
+
+print("a) ", min(2, 6, 8, 5))
+print("b) ", bin(46))
+print("c) ", round(10.544336, 2))
+print("d) ", math.hypot(6, 8))
+print("e) ", math.modf(3.1415))
